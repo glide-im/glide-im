338db515e72b08704faaa85bd5e0e79d8875b511
degnzii <dengzii@foxmail.com>
2021-08-13 10:59:08 +0800

    update github action

--- build-linux64.bat
+++
@@ -1,5 +0,0 @@
-del blogger
-SET CGO_ENABLED=0
-SET GOOS=linux
-SET GOARCH=amd64
-go build -o blogger ./main/main.go
--- build-windows64.bat
+++
@@ -1,5 +0,0 @@
-del blogger
-SET CGO_ENABLED=0
-SET GOOS=windows
-SET GOARCH=amd64
-go build -o blogger.exe ./main/main.go
