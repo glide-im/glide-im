06855515c54e2f2b32f490129918977f62b8c963
degnzii <dengzii@foxmail.com>
2021-11-01 18:03:58 +0800

    feat: timingwheel

--- pkg/timingwheel/timingwheel.go
+++ pkg/timingwheel/timingwheel.go
@@ -1,38 +1,166 @@
 package timingwheel
 
 import (
-	"sync"
+	"fmt"
+	"go_im/pkg/logger"
+	"math"
 	"time"
 )
 
-type TimingWheel struct {
-	m sync.Mutex
+type value struct {
+	name   string
+	offset int
+	c      chan struct{}
 
+	start int64
+}
+
+func (s *value) String() string {
+	return fmt.Sprintf("value{name=%s,offset=%d,c=%values}", s.name, s.offset, s.c)
+}
+
+func (s *value) call() {
+	s.c <- struct{}{}
+}
+
+type slot struct {
+	index  int
+	next   *slot
+	len    int
+	values map[*value]interface{}
+
+	circulate bool
+}
+
+func newSlot(circulate bool, len int) *slot {
+	slot1 := &slot{
+		len:       len,
+		values:    map[*value]interface{}{},
+		circulate: circulate,
+	}
+	var s = slot1
+	for i := 1; i < len; i++ {
+		n := &slot{
+			index:     i,
+			len:       len,
+			values:    map[*value]interface{}{},
+			circulate: circulate,
+		}
+		s.next = n
+		s = n
+	}
+	s.next = slot1
+	return slot1
+}
+
+func (s *slot) put(offset int, v *value) int {
+	if offset <= 0 {
+		s.values[v] = nil
+		return 0
+	}
+	if offset >= s.len {
+		return offset - s.len
+	}
+	if !s.circulate && (s.index+offset) > s.len-1 {
+		return (s.index + offset) - (s.len - 1)
+	}
+	return s.next.put(offset-1, v)
+}
+
+func (s *slot) isEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *slot) callAndRm() {
+	if s.isEmpty() {
+		return
+	}
+	for k := range s.values {
+		k.call()
+		delete(s.values, k)
+	}
+}
+
+func (s *slot) remove(v *value) {
+	delete(s.values, v)
+}
+
+func (s *slot) valueArray() []*value {
+	var r []*value
+	for k := range s.values {
+		r = append(r, k)
+	}
+	return r
+}
+
+type wheel struct {
+	slotCap int
+
+	slot  *slot
+	child *wheel
+}
+
+func newWheel(buckets int, dep int, wheels int, child *wheel) *wheel {
+	wh := &wheel{
+		slot:    newSlot(dep == wheels, buckets),
+		slotCap: int(math.Pow(float64(buckets), float64(dep))) / buckets,
+		child:   child,
+	}
+	return wh
+}
+
+func (w *wheel) move() bool {
+	if w.child != nil {
+		if w.child.move() {
+			w.slot = w.slot.next
+		}
+		if !w.slot.isEmpty() {
+			for _, v := range w.slot.valueArray() {
+				v.offset = v.offset % w.slotCap
+				w.slot.remove(v)
+				w.child.put(v)
+			}
+		}
+	} else {
+		w.slot.callAndRm()
+		w.slot = w.slot.next
+	}
+	if w.slot.next.index == 0 {
+		w.slot = w.slot.next
+		return true
+	} else {
+		return false
+	}
+}
+
+func (w *wheel) put(v *value) {
+	offset := v.offset / w.slotCap
+	r := w.slot.put(offset, v)
+	if r > 0 {
+		logger.E("put failed", r, v)
+	}
+}
+
+type TimingWheel struct {
 	interval   time.Duration
 	ticker     *time.Ticker
 	quit       chan struct{}
 	maxTimeout time.Duration
 
-	cs  []chan struct{}
-	pos int
+	wheel *wheel
 }
 
 func NewTimingWheel(interval time.Duration, buckets int) *TimingWheel {
 	w := new(TimingWheel)
 
-	w.m = sync.Mutex{}
 	w.interval = interval
 	w.quit = make(chan struct{})
-	w.pos = 0
-
-	w.maxTimeout = interval * (time.Duration(buckets))
-
-	w.cs = make([]chan struct{}, buckets)
+	w.maxTimeout = interval * (time.Duration(buckets ^ 3))
 	w.ticker = time.NewTicker(interval)
 
-	for i := range w.cs {
-		w.cs[i] = make(chan struct{})
-	}
+	w1 := newWheel(buckets, 1, 3, nil)
+	w2 := newWheel(buckets, 2, 3, w1)
+	w.wheel = newWheel(buckets, 3, 3, w2)
 	go w.run()
 
 	return w
@@ -46,18 +174,14 @@ func (w *TimingWheel) After(timeout time.Duration) <-chan struct{} {
 	if timeout >= w.maxTimeout {
 		panic("timeout too much, over max timeout")
 	}
-
 	index := int(timeout / w.interval)
-	if 0 < index {
-		index--
+	ch := make(chan struct{})
+	s := &value{
+		offset: index,
+		c:      ch,
 	}
-
-	w.m.Lock()
-	index = (w.pos + index) % len(w.cs)
-	b := w.cs[index]
-	w.m.Unlock()
-
-	return b
+	w.wheel.put(s)
+	return nil
 }
 
 func (w *TimingWheel) run() {
@@ -73,10 +197,5 @@ func (w *TimingWheel) run() {
 }
 
 func (w *TimingWheel) onTicker() {
-	w.m.Lock()
-	lastC := w.cs[w.pos]
-	w.cs[w.pos] = make(chan struct{})
-	w.pos = (w.pos + 1) % len(w.cs)
-	w.m.Unlock()
-	close(lastC)
+	w.wheel.move()
 }
--- pkg/timingwheel/timingwheel_test.go
+++ pkg/timingwheel/timingwheel_test.go
@@ -1,16 +1,61 @@
 package timingwheel
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 )
 
-func TestTimingWheel_After(t *testing.T) {
+func TestNewTimingWheel(t *testing.T) {
 
-	tw := NewTimingWheel(time.Second, 10)
+	tw := NewTimingWheel(time.Second*1, 20)
+	after := tw.After(time.Second * 3)
+	tk := time.NewTicker(time.Second)
+	c := 0
 
-	for range tw.After(time.Second * 3) {
-		t.Log("#")
+	status := func() {
+		var w = tw.wheel
+		for w != nil {
+			t.Log(w.status())
+			w = w.child
+		}
 	}
-	t.Log("done")
+	for {
+		select {
+		case <-after:
+			t.Log("done")
+		case <-tk.C:
+			c++
+			t.Log("=>", c)
+			status()
+		}
+	}
+}
+
+func (w *wheel) status() string {
+	var s []string
+	sl := w.slot
+	for ; sl.index != sl.len-1; sl = sl.next {
+
+	}
+	for i := 0; i != sl.len; i++ {
+		sl = sl.next
+		if sl == w.slot {
+			s = append(s, "*")
+			continue
+		}
+		if sl.isEmpty() {
+			s = append(s, "_")
+		} else {
+			s = append(s, "#")
+		}
+	}
+	return fmt.Sprintf("value=%v", s)
+}
+
+func sleepRndMilleSec(start int32, end int32) {
+	n := rand.Int31n(end - start)
+	n = start + n
+	time.Sleep(time.Duration(n) * time.Millisecond)
 }
